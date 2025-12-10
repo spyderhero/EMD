a1668697ef35715025c67323c9cfe7c5fe51e199
Ryoto Tomie <beige740@gmail.com>
2025-12-10 12:38:57 +0900

    delete debug

--- QuasiLocalMeasuresEMD/src/qlm_emd_charge.F90
+++ QuasiLocalMeasuresEMD/src/qlm_emd_charge.F90
@@ -26,7 +26,6 @@ subroutine qlm_emd_compute_charge (CCTK_ARGUMENTS, hn)
 
   charge_local = 0.0
 
-  call CCTK_INFO ("Start calc charege")
   do j = 1, qlm_emd_nphi(hn)-1
     do i = 1, qlm_emd_ntheta(hn)-1
 
@@ -76,7 +75,7 @@ subroutine qlm_emd_compute_charge (CCTK_ARGUMENTS, hn)
     end do
   end do
 
-  call CCTK_INFO ("finish calc local charege")
+  
   ! Divide by 4π
   qlm_emd_electric_charge(hn) = charge_local / (4.0*pi)
 
