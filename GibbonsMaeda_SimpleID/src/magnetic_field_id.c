25a246ed27d08c45b7515a34403494f32aa406f3
Ryoto Tomie <beige740@gmail.com>
2025-12-12 16:49:15 +0900

    fix vector potential

--- GibbonsMaeda_SimpleID/src/magnetic_field_id.c
+++ GibbonsMaeda_SimpleID/src/magnetic_field_id.c
@@ -100,8 +100,8 @@ void MagneticField (CCTK_ARGUMENTS)
         Ax[ind]    = par_qm_plus * z1 * y1 / r_plus / sqrt(pow(x1 - par_b,2) + pow(y1,2)) 
                     + par_qm_minus * z1 * y1 / r_plus / sqrt(pow(x1 + par_b,2) + pow(y1,2));
 
-        Ay[ind]    = par_qm_plus * z1 * x1 / r_plus / sqrt(pow(x1 - par_b,2) + pow(y1,2)) 
-                    + par_qm_minus * z1 * x1 / r_plus / sqrt(pow(x1 + par_b,2) + pow(y1,2));
+        Ay[ind]    = -par_qm_plus * z1 * x1 / r_plus / sqrt(pow(x1 - par_b,2) + pow(y1,2)) 
+                    - par_qm_minus * z1 * x1 / r_plus / sqrt(pow(x1 + par_b,2) + pow(y1,2));
                     
         Az[ind]    = 0;
 
